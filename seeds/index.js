3f8df0442c3bcebcf2c0efe17311d752e9861352
Anton <antonbogatirev1@gmail.com>
2021-01-21 10:44:15 -0800

    Database and show page update for images, price and description.
    Database now takes images.
    Added a random image to each entry through a Unsplash collection.
    Added a random price and a lorem description to each entry.
    Updated our show.ejs page to show the new data.
    Changed price from String to Number in our Model.
    Ran the seeds file and generated a new random database.

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedhelpers');
+const { places, descriptors } = require('./seedhelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -21,11 +21,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`        
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim libero maxime culpa ratione nemo rem explicabo quibusdam et consequuntur impedit placeat ullam dolorem temporibus quis excepturi eveniet modi, obcaecati vero?',
+            price
         })
-        await camp.save();  
+        await camp.save();
     }
 }
 
