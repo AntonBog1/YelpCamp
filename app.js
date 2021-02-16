1e25e1faec312186c43fc32fd8d9866e21a648dc
Anton <antonbogatirev1@gmail.com>
2021-02-16 13:07:26 -0800

    Added Sessions
    Installed express-session.
    Configured it to remove fix all deprecation warnings.
    Set "htttpOnly: true' for security reasons.
    Set the session to expire in one week.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -33,6 +34,18 @@ app.use(methodOverride('_method'));
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+app.use(session(sessionConfig));
 
 app.get('/', (req, res) => {
     res.render('home');
