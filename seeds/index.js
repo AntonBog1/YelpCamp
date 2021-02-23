c2ce3cefccf4add175b4e2b7bcf6e50df73ebbcc
Anton <antonbogatirev1@gmail.com>
2021-02-22 19:15:10 -0800

    Campground Authors, Authorization and Permissions
    Deleted campground database.
    Added a author section to the campground model.
    Added a author section to the review model.
    Added a userId of one current user in our database to the seeds file.
    Ran the seeds file to generate a new campgrounds database, now all campgrounds have an author.
    The show page now shows the username of who created that campground.
    Creating a new campground will also save who created that campground.
    Users can now only delete and edit their own campgrounds.
    Moved some middleware into its own file.
    The review form on the show page will only appear when a user is logged in.
    Creating a review will now save the user's ID.
    The show page will now show the username of the person who left a review.
    Reviews may only be deleted by the person who wrote the review.

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '603152cbe990d7b0c9acd93b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
