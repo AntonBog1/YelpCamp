31def199da36e87654724c25a8e20c492939a962
Anton <antonbogatirev1@gmail.com>
2021-03-11 12:13:46 -0800

    Centering Mapbox on Campground Location
    Add functionality that automatically centers the Mapbox map to that campground's location.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 4
+    center: JSON.parse(campground),
+    zoom: 10
 });
+
+new mapboxgl.Marker()
+    .setLngLat(JSON.parse(campground))
+    .addTo(map);
