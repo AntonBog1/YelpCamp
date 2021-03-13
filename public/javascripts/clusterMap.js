f695a61044b3dca8750e17f171979ce7de3cecc4
Anton <antonbogatirev1@gmail.com>
2021-03-13 13:28:36 -0800

    Customizing Cluster Map Pop Up Markers
    Created a properties.popUpMarkup virtual on the campgroundSchema for MapBox.
    Allowed JSON virtuals on the campgroundSchema.
    Customized the Cluster Map Pop Up to show the title and description that links to that campground.
    Added a mouse pointer when hovering over a campground on the Cluster Map.
    Updated some variables from Var to Const.

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken = mapToken;
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
@@ -79,10 +79,10 @@ map.on('load', function () {
 
     // inspect a cluster on click
     map.on('click', 'clusters', function (e) {
-        var features = map.queryRenderedFeatures(e.point, {
+        const features = map.queryRenderedFeatures(e.point, {
             layers: ['clusters']
         });
-        var clusterId = features[0].properties.cluster_id;
+        const clusterId = features[0].properties.cluster_id;
         map.getSource('campgrounds').getClusterExpansionZoom(
             clusterId,
             function (err, zoom) {
@@ -101,15 +101,9 @@ map.on('load', function () {
     // the location of the feature, with
     // description HTML from its properties.
     map.on('click', 'unclustered-point', function (e) {
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var mag = e.features[0].properties.mag;
-        var tsunami;
+        const { popUpMarkup } = e.features[0].properties;
+        const coordinates = e.features[0].geometry.coordinates.slice();
 
-        if (e.features[0].properties.tsunami === 1) {
-            tsunami = 'yes';
-        } else {
-            tsunami = 'no';
-        }
 
         // Ensure that if the map is zoomed out such that
         // multiple copies of the feature are visible, the
@@ -120,9 +114,7 @@ map.on('load', function () {
 
         new mapboxgl.Popup()
             .setLngLat(coordinates)
-            .setHTML(
-                'magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami
-            )
+            .setHTML(popUpMarkup)
             .addTo(map);
     });
 
@@ -130,6 +122,12 @@ map.on('load', function () {
         map.getCanvas().style.cursor = 'pointer';
     });
     map.on('mouseleave', 'clusters', function () {
+        map.getCanvas().style.cursor = '';
+    });
+    map.on('mouseenter', 'unclustered-point', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'unclustered-point', function () {
         map.getCanvas().style.cursor = '';
     });
 });
