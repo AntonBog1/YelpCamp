0719c2e431c665b8c2160578567ab85c3a69aa04
Anton <antonbogatirev1@gmail.com>
2021-03-19 15:08:20 -0700

    Fixing Deployment Errors
    Added a start script.
    Updated with port we are serving on.

--- app.js
+++ app.js
@@ -150,6 +150,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('LIVE ON PORT 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`LIVE ON PORT ${port}`);
 });
